0654db9b40cdb8da6cd1b6931fb6a06b3dc481e0
rbpolim <rodrigobpolim@gmail.com>
2020-10-29 11:52:50 -0300

    all tests ok

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories]= useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    // TODO
+    const response = await api.post('repositories', {
+      title: `Repositório ${Date.now()}`,
+      url: 'teste',
+      techs: 'teste',
+      likes: 0,
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+    const repositoryIndex = repositories.findIndex(
+      repository => repository.id === id
+    );
+
+    repositories.splice(repositoryIndex, 1)
+
+    setRepositories([...repositories])
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)} >
+              Remover
+            </button>
+          </li>
+        )}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
